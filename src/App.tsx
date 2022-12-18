86bd29dca10d6d2d03bdaf4c52347ae22c148112
felipematheus1337 <lipehbr@gmail.com>
2022-12-18 11:20:45 -0300

    project created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="menu">
+      <h4>Home</h4>
+      <h4>Show</h4>
+      <h4>Search</h4>
+      <h4>About</h4>
+   </div>
   );
 }
 
