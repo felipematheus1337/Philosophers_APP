43b56440bd278c5f811f3ae1370689b66a930962
felipematheus1337 <lipehbr@gmail.com>
2022-12-18 11:31:04 -0300

    menu finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUnorderedList } from 'react-icons/ai';
+import { AiOutlineSearch } from 'react-icons/ai'
+import {AiOutlineInfoCircle} from 'react-icons/ai'
 
 function App() {
   return (
     <div className="menu">
-      <h4>Home</h4>
-      <h4>Show</h4>
-      <h4>Search</h4>
-      <h4>About</h4>
+      <h4><AiOutlineHome size={20}/>Home</h4>
+      <h4><AiOutlineUnorderedList size={20} />List</h4>
+      <h4><AiOutlineSearch size={20} />Search</h4>
+      <h4><AiOutlineInfoCircle size={20} />About</h4>
    </div>
   );
 }
