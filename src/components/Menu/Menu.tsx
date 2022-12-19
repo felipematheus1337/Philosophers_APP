62310d0ad0169c779a5f8bf0ba530c33e8353988
felipematheus1337 <lipehbr@gmail.com>
2022-12-19 20:59:23 -0300

    home page finished

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,15 +3,26 @@ import './styles.css';
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineUnorderedList } from 'react-icons/ai';
 import { AiOutlineSearch } from 'react-icons/ai'
-import {AiOutlineInfoCircle} from 'react-icons/ai'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Menu() {
+
+  const [toSearch, setToSearch] = useState<Boolean>(false);
+  const [paramToFind, setParamToFind] = useState<string>();
+
   return (
     <div className="menu">
-      <h4><AiOutlineHome size={20}/>Home</h4>
-      <h4><AiOutlineUnorderedList size={20} />List</h4>
-      <h4><AiOutlineSearch size={20} />Search</h4>
-      <h4><AiOutlineInfoCircle size={20} />About</h4>
+      <Link to="/"><h4><AiOutlineHome size={20}/>Home</h4></Link>
+     <Link to="/list"><h4><AiOutlineUnorderedList size={20} />List</h4></Link> 
+      <h4 onClick={() => {
+        setToSearch(!toSearch);
+      }}><AiOutlineSearch size={20} />Search</h4>
+      {toSearch && <div className="search"> <input type="text" onChange={(e) => setParamToFind(e.target.value)} />
+      <button>Find</button>
+      </div>}
+      <Link to="/about"><h4><AiOutlineInfoCircle size={20} />About</h4></Link>
    </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./styles.css";
+import Gip from "../../assets/imgs/gip.gif"
 
 function Home() {
   return (
       <div className="home">
-          <h1>Home Page</h1>
+      <h2>Welcome to the Philosophers APP!</h2>
+      <span>An APP Created by somebody who loves Philosophy</span>
+      <span>The intentional was to synthesize my knowledge on programming and philosophy </span>
+     <span><img src={Gip}/><h1>Enjoy, IT!</h1></span>
+      
      </div>
   );
 }
