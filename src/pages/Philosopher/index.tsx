7cf3a0937dd3349b7d5addae538145bd8552cf71
felipematheus1337 <lipehbr@gmail.com>
2022-12-28 20:47:35 -0300

    adjusts to philosopher page

--- src/pages/Philosopher/index.tsx
+++ src/pages/Philosopher/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { useLocation } from "react-router-dom";
+import {mockWorks} from "../../../src/@types/types";
 
 
 const Philosopher = () => {
@@ -8,11 +9,34 @@ const Philosopher = () => {
     const { state } = useLocation();
     const { philosopher } = state || {};
 
+
     console.log(philosopher);
 
     return (
-        <div>
-            Hello {Philosopher.name}!   
+        <div className="container-philosopher">
+            <img src={philosopher.image} alt={philosopher.name}/>
+            <h3>{philosopher.name}, {philosopher.country} {philosopher.birthDate}</h3>
+            <div className="content-philosopher">
+            <div className="about-philosopher">
+              <h4>About</h4>
+              <p>Philosophy Type: {philosopher.typePhilosophy}</p>
+              <p>Flag: <img crossOrigin="anonymous" src={philosopher.flag} alt="flag"/></p>
+              <p>{philosopher.description}</p>
+            </div>
+            <aside className="works-philosopher">
+                <h4>Works</h4>
+               {philosopher.works && philosopher.works.map((work:mockWorks) => {
+                return(
+                    <>
+                    <p>{work.name}</p>
+                    <p>{work.date}</p>
+                    <a href={work.buyUrl}>Click here to buy it!</a>
+                    <hr />
+                    </>
+                )
+               })}
+            </aside>
+            </div>
         </div>
     )
 }
